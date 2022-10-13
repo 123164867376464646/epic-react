88bc8502f9f215dbdce03a38590eb3c8286aaa6f
linmumu <1054742101@qq.com>
2022-10-13 11:14:38 +0800

    使用antd的upload组件实现上传

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,30 +1,47 @@
-import React, {useRef} from "react";
+import React from "react";
 import {useStores} from "../stores";
 import {observer} from "mobx-react";
+import {InboxOutlined} from '@ant-design/icons';
+import {Upload} from 'antd';
+
+const {Dragger} = Upload;
+
 
 export const Uploader = observer(() => {
   const {ImageStore} = useStores()
-  const refInput = useRef()
-  const bindChange = () => {
-    console.log(refInput.current)
-    if (refInput.current.files.length > 0) {
-      ImageStore.setFilename(refInput.current.files[0].name)
-      ImageStore.setFile(refInput.current.files[0])
+  const props = {
+    showUploadList: false,
+    beforeUpload: file => {
+      ImageStore.setFilename(file.name)
+      ImageStore.setFile(file)
       ImageStore.upload()
         .then((serverFile) => {
           console.log('上传成功')
           console.log(serverFile)
-
         }).catch((err) => {
         console.error(err)
       })
+      return false
     }
-    window.file = refInput.current
   }
   return (
     <div>
-      <h1>文件上传</h1>
-      <input type='file' ref={refInput} onChange={bindChange}/>
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined/>
+        </p>
+        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+          band files
+        </p>
+      </Dragger>
+      <div>
+        <h1>上传结果</h1>
+        {
+          ImageStore.serverFile ? <div>{ImageStore.serverFile.attributes.url.attributes.url}</div> : null
+        }
+      </div>
     </div>
   )
 })
