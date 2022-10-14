8aed10fc6fa9d94bdedae3d0f1e4a55898b6d519
linmumu <1054742101@qq.com>
2022-10-15 00:51:10 +0800

    一键部署 deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+
+yarn build &&
+cd build &&
+git init &&
+git add . &&
+git commit -m deploy &&
+git remote add origin git@gitee.com:linmumujiayou/epic-react.git &&
+git push -u -f origin master &&
+cd ..;
